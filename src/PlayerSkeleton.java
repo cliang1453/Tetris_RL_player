a208136207939f155b1a19a82f71c4b3810ce1f4
Chen Liang <cliang73@gatech.edu>
2019-03-15 20:30:04 -0400

    java-side player init

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -6,37 +6,69 @@ import py4j.GatewayServer;
 
 public class PlayerSkeleton {
 	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
-		Random generator = new Random();
-		int randomIndex = generator.nextInt(legalMoves.length);
-		return randomIndex;
+
+	private int slot; 
+	private int orient;
+	private State s;
+	private TFrame t; 
+	private List<BenchListener> listeners = new ArrayList<BenchListener>();
+	private PlayerSkeleton p = new PlayerSkeleton();
+
+	// public int pickMove(State s, int[][] legalMoves) {
+	// 	Random generator = new Random();
+	// 	int randomIndex = generator.nextInt(legalMoves.length);
+	// 	return randomIndex;
+	// }
+
+	public int[] getAction() {
+		int[] action = {orient, slot};
+		return action;
 	}
 
-	public int testMove(int test) {
-		return test;
+	public int getState(){
+		return s;
 	}
+
+
+	public void startGame(int count) {
+		System.out.println("PlayerSkeleton:startGame...");
+		for (int i = 0; i < count; i++) {
+			System.out.println("In startGame"+i);
+			EventProducer.start_game(this);
+		}
+	}
+
+	public void takeAction(int orient, int slot) {
+		System.out.println("PlayerSkeleton:takeAction" + slot + " " + orient);
+		this.slot = slot; 
+		this.orient = orient;
+	}
+
 	
+
+	public void notifyAllListeners() {
+		System.out.println("	SmallBench:notifyAllListeners");
+		
+		try {
+			int next_piece = s.getNextPiece();
+			int[][] field = s.getField();
+			boolean is_end = p.hasLost();
+			for (BenchListener listener : listeners) {
+				listener.notify(next_piece, field, is_end);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
 	public static void main(String[] args) {
 
-		PlayerSkeleton p = new PlayerSkeleton();
-		State s = new State();
-		TFrame t = new TFrame(s);
-
-		while(!s.hasLost()) {
-			s.makeMove(p.pickMove(s,s.legalMoves()));
-			s.draw();
-			s.drawNext(0,0);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println(s.getRowsCleared());
-
-		// app is now the gateway.entry_point
 		GatewayServer server = new GatewayServer(p);
 		server.start();
-
+		
+		s = new State();
+		t = new TFrame(s);
 	}
 }
