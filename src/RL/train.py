1f8a7386984ee3a68e935851605799324299d671
yetongumich <yetong@umich.edu>
2019-03-23 18:18:11 -0400

    add plot and save model

--- src/RL/train.py
+++ src/RL/train.py
@@ -25,8 +25,9 @@ def parse_args():
     parser.add_argument("--num-episodes", type=int, default=1000, help="number of episodes")
     parser.add_argument("--num-games", type=int, default=1000, help="number of games")
     parser.add_argument("--batch-size", type=int, default=32, help="number of games")
-    parser.add_argument("--save-dir", type=str, default="", help="directory to save policy and plots")
+    parser.add_argument("--save-dir", type=str, default="log", help="directory to save policy and plots")
     parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
+    parser.add_argument("--max-capacity", type=int, default=1000, help="maximum capacity of replay buffer")
 
     args = parser.parse_args()
     return args
@@ -45,9 +46,16 @@ class PythonListener(object):
         self.gateway = gateway
         self.policy = policy
         self.replay_buffer = replay_buffer
+        self.game_count = 0
+
+    def is_valid(self):
+        if self.game_count % 10 == 0:
+            return True
+        else:
+            return False
 
     def notify(self, next_piece, field, rows_cleared, is_end):
-        print(colored("=" * 40 + " notify " + "=" * 40, 'red'))
+        print(colored("=" * 40 + str(self.game_count) + "=" * 40, 'red'))
         # format state
         print("\tnext_piece\t", next_piece)
         print("\tis_end\t", is_end)
@@ -63,23 +71,29 @@ class PythonListener(object):
         if is_end:
             action = [0, 0]
         else:
-            action = self.policy.take_action(state)
+            action = self.policy.take_action(state, is_valid=self.is_valid())
 
-        # store transition
-        print("\t adding to replay buffer...")
-        self.replay_buffer.add(state, action, rows_cleared, is_end)
+        if not self.is_valid():
+            # store transition
+            print("\t adding to replay buffer...")
+            self.replay_buffer.add(state, action, rows_cleared, is_end)
 
-        if self.replay_buffer.get_size() % 20 == 0:
-            self.replay_buffer.visualize_replaybuffer()
+            if self.replay_buffer.get_size() % 20 == 0:
+                self.replay_buffer.visualize_replaybuffer()
 
-        # sample and update
-        if len(self.replay_buffer.valid_idx_list) > self.args.batch_size:
-            print("\tsampling...")
-            samples = self.replay_buffer.sample(self.args.batch_size)
-            self.policy.learn(samples)
+            # sample and update
+            if len(self.replay_buffer.valid_idx_list) > self.args.batch_size:
+                print("\tsampling...")
+                samples = self.replay_buffer.sample(self.args.batch_size)
+                self.policy.learn(samples)
 
         # run simulation step
         print("\t calling java with action", action)
+        if is_end:
+            self.game_count += 1
+            if self.is_valid():
+                self.policy.save_params()
+
         self.gateway.entry_point.takeAction(int(action[0]), int(action[1]))
 
     class Java:
@@ -136,19 +150,19 @@ def simulate_drop(state, action):
 class Policy:
     def __init__(self, args):
         self.args = args
+        self.logger = args.logger
         self.q_func = QFunc(args)
         self.optimizer = optim.RMSprop(self.q_func.parameters(), lr=args.lr, alpha=args.alpha, eps=args.eps)
-        self.logger = LossLogger()
         return
 
-    def take_action(self, state):
+    def take_action(self, state, is_valid=True):
 
         print("\ttake_action:")
 
         action_space = get_action_space(state[1])
 
         # epsilon greedy
-        if random.random() < self.args.epsilon_g:
+        if not is_valid and (random.random() < self.args.epsilon_g):
             print("taking random action")
             action = random.choice(action_space)
             print(action)
@@ -211,29 +225,50 @@ class Policy:
         loss.backward()
         self.optimizer.step()
 
+    def save_params(self):
+        torch.save(self.q_func.state_dict(), os.path.join(self.args.save_dir, "mymodel"))
 
-class LossLogger:
-    def __init__(self):
+
+class Logger:
+    def __init__(self, args):
+        self.args = args
         self.loss_list = []
+        self.reward_list = []
+        self.loss_figure = plt.figure("loss")
+        self.reward_figure = plt.figure("reward")
 
     def add_loss(self, loss):
         self.loss_list.append(loss)
 
+    def add_reward(self, reward):
+        self.reward_list.append(reward)
+
     def plot_loss(self):
+        plt.figure('loss')
         plt.plot(self.loss_list)
-        plt.show()
+        plt.savefig(os.path.join(self.args.save_dir, 'loss.png'))
+
+    def plot_reward(self):
+        plt.figure('reward')
+        plt.plot(self.reward_list)
+        plt.savefig(os.path.join(self.args.save_dir, 'reward.png'))
 
 
 class ReplayBuffer:
-    def __init__(self, max_capacity=1000):
+    def __init__(self, args):
+        self.args = args
         self.state_list = []
         self.action_list = []
         self.cleared_list = []
         self.is_end_list = []
+        self.reward_list = []
         self.count = 0
         self.valid_idx_list = []
         self.start_index = 0
-        self.max_capacity = max_capacity
+        self.prev_end = -1
+
+    def rel_index(self, index):
+        return index - self.start_index
 
     def get_size(self):
         return len(self.valid_idx_list)
@@ -244,12 +279,23 @@ class ReplayBuffer:
         self.cleared_list.append(rows_cleared)
         self.is_end_list.append(is_end)
 
+        # calculate reward
+        if self.count == self.prev_end + 1:
+            self.reward_list.append(0)
+        else:
+            self.reward_list.append(self.calc_reward(self.rel_index(self.count)))
+
         if not is_end:
             self.valid_idx_list.append(self.count)
-        self.count += 1
+
+        # cacluate accumulative reward
+        if is_end:
+            reward_sum = sum(self.reward_list[self.rel_index(self.prev_end) + 1: self.rel_index(self.count)])
+            self.args.logger.add_reward(reward_sum)
+            self.prev_end = self.count
 
         # keep replay buffer udner capacity
-        if len(self.state_list) > self.max_capacity:
+        if len(self.state_list) > self.args.max_capacity:
             self.state_list = self.state_list[100:]
             self.action_list = self.action_list[100:]
             self.cleared_list = self.cleared_list[100:]
@@ -258,13 +304,15 @@ class ReplayBuffer:
             idx = np.argmax(self.valid_idx_list >= self.start_index)
             self.valid_idx_list = self.valid_idx_list[idx:]
 
+        self.count += 1
+
     def sample(self, num_samples):
         indices = np.random.choice(self.valid_idx_list[:-1], num_samples, replace=False)
         indices -= self.start_index
         sampled_states = [self.state_list[i] for i in indices]
         sampled_actions = [self.action_list[i] for i in indices]
         sampled_next_state = [self.state_list[i + 1] for i in indices]
-        sampled_rewards = [self.calc_reward(i) for i in indices]
+        sampled_rewards = [self.reward_list[i + 1] for i in indices]
         sampled_is_end = [self.is_end_list[i + 1] for i in indices]
         return [sampled_states, sampled_actions, sampled_next_state, sampled_rewards, sampled_is_end]
 
@@ -276,25 +324,12 @@ class ReplayBuffer:
         print("sampled_rewards", sampled_rewards)
 
     def calc_reward(self, i):
-        if self.is_end_list[i + 1]:
-
+        if self.is_end_list[i]:
             reward = -10
         else:
-            reward = 1 + (self.cleared_list[i + 1] - self.cleared_list[i]) * 5
+            reward = 1 + (self.cleared_list[i] - self.cleared_list[i - 1]) * 5
         return reward
 
-
-class Logger:
-    def __init__(self, args):
-        self.args = args
-        self.reward_list = []
-
-    def add(self, reward):
-        self.reward_list.append(reward)
-
-    def plot(self):
-        plt.plot(self.reward_list)
-        plt.savefig(os.path.join(self.args.save_dir, "reward.png"))
 
 
 def calculate_reward(state, next_state, is_end):
@@ -311,16 +346,16 @@ def collect_data(args, policy, replay_buffer, num_games=1):
 
 def main():
     args = parse_args()
-    logger = Logger(args)
+    args.logger = Logger(args)
     policy = Policy(args)
-    replay_buffer = ReplayBuffer()
+    replay_buffer = ReplayBuffer(args)
     collect_data(args, policy, replay_buffer, num_games=args.num_games)
 
 
 def run_heuristic():
     args = parse_args()
     policy = HeuristicPolicy(args)
-    replay_buffer = ReplayBuffer()
+    replay_buffer = ReplayBuffer(args)
     collect_data(args, policy, replay_buffer, num_games=args.num_games)
 
 
@@ -328,7 +363,7 @@ def test_policy():
     args = parse_args()
     logger = Logger(args)
     policy = Policy(args)
-    replay_buffer = ReplayBuffer()
+    replay_buffer = ReplayBuffer(args)
 
     for i in range(2):
 
